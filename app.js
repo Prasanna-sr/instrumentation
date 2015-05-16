f87223a3ed8e000adb8f1c3292e8c1804c170d58
prasanna ramanujam <prasannaraajkumar@gmail.com>
2015-05-16 16:46:50 -0700

    response time captured for all middlewares

--- app.js
+++ app.js
@@ -5,29 +5,37 @@ var router = express.Router();
 var captureTime = require('./captureTime');
 captureTime(app); 
 
-app.use(function prkApp2(req, res, next) {
+app.use(function prkApp1(req, res, next) {
 	next();
 });
 app.use(router);
+
+app.use(function timerssss(req,res, next){
+	console.log(req.timers);
+	next();
+});
 
 app.get('/apptest', function(req, res, next) {
 	res.send('apptest');
 	next();
 });
-router.use(function prkRouter(req, res, next) {
+router.use(function prkRouter1(req, res, next) {
 	console.log('prkRouter');
-	next();
+	setTimeout(function() {
+		next();
+	}, 200);
+	
 });
 
-router.use(function prkRouter1(req, res, next) {
+router.use(function prkRouter2(req, res, next) {
 	console.log('prkRouter1');
-	var a = "";
+	var a = '';
 	for(var i = 0; i< 100; i++) {
 		a = a + i;
 	}
 	next();
-});
-router.get('/test', function(req, res, next) {
+}); 
+router.get('/test', function test(req, res, next) {
 	res.send('test');
 	next();
 });
--- captureTime.js
+++ captureTime.js
@@ -1,69 +1,115 @@
 module.exports = function(app) {
-    overrideMethod(app, 'use', function(appUseFn) {
+    overrideMethod(app, 'use', appMiddleware);
+    overrideMethod(app, 'get', routerHttpMethods);
+    overrideMethod(app, 'post', routerHttpMethods);
+    overrideMethod(app, 'put', routerHttpMethods);
+    overrideMethod(app, 'delete', routerHttpMethods);
+
+    function appMiddleware(appUseFn) {
         return function() {
             var appFn = arguments[0];
             if (typeof appFn === 'function' && typeof appFn.use === 'function') {
-                handleRouterMiddleware(appFn);
-            } else {
+                handleRouterMethods(appFn);
+            } else if (typeof appFn === 'function') {
                 arguments[0] = function() {
+                    var $obj = {};
+                    $obj["timerObj"] = {};
+                    $obj.req = arguments[0];
+                    $obj.counter = new Date().getTime();
+                    if (!arguments[0].timers) {
+                        arguments[0].timers = [];
+                    }
                     console.log('application start time : ' + appFn.name);
                     var nextFn = arguments[2];
                     arguments[2] = function() {
                         console.log('application end time : ' + appFn.name);
+                        if (appFn.name) {
+                            $obj["timerObj"][appFn.name] = new Date().getTime() - $obj.counter;
+                        } else {
+                            $obj["timerObj"]["anonymous"] = new Date().getTime() - $obj.counter;
+                        }
+                        $obj.req.timers.push($obj["timerObj"]);
                         nextFn.apply(this, arguments);
+                        
+                        
                     }
                     return appFn.apply(this, arguments);
                 }
             }
             return appUseFn.apply(this, arguments);
         }
-    });
-    overrideMethod(app, 'get', routerHttpMethods);
-    overrideMethod(app, 'post', routerHttpMethods);
-    overrideMethod(app, 'put', routerHttpMethods);
-    overrideMethod(app, 'delete', routerHttpMethods);
+    }
 
     //router is used or sub middleware are used
-    function handleRouterMiddleware(router) {
-        overrideMethod(router, "use", function(routerUse) {
-            return function() {
-                var routerFn = arguments[0];
-                arguments[0] = function() {
-                    console.log('router start time ' + routerFn.name);
-                    var nextFn = arguments[2];
-                    arguments[2] = function() {
-                        console.log('router end time ' + routerFn.name);
-                        nextFn.apply(this, arguments);
-                    }
-                    return routerFn.apply(this, arguments);
-                }
-                return routerUse.apply(this, arguments);
-            }
-        });
+    function handleRouterMethods(router) {
+        overrideMethod(router, "use", routerMiddleware);
         overrideMethod(router, 'get', routerHttpMethods);
         overrideMethod(router, 'post', routerHttpMethods);
         overrideMethod(router, 'put', routerHttpMethods);
         overrideMethod(router, 'delete', routerHttpMethods);
     }
 
+    function routerMiddleware(routerUse) {
+        return function() {
+            var routerFn = arguments[0];
+            arguments[0] = function() {
+                var $obj = {};
+                $obj["timerObj"] = {};
+                $obj.req = arguments[0];
+                $obj.counter = new Date().getTime();
+                if (!arguments[0].timers) {
+                    arguments[0].timers = [];
+                }
+                console.log('router start time ' + routerFn.name);
+                var nextFn = arguments[2];
+                arguments[2] = function() {
+                    console.log('router end time ' + routerFn.name);
+                    if (routerFn.name) {
+                        $obj["timerObj"][routerFn.name] = new Date().getTime() - $obj.counter;
+                    } else {
+                        $obj["timerObj"]["anonymous"] = new Date().getTime() - $obj.counter;
+                    }
+                    $obj.req.timers.push($obj["timerObj"]);
+                    nextFn.apply(this, arguments);
+                    
+                }
+                return routerFn.apply(this, arguments);
+            }
+            return routerUse.apply(this, arguments);
+        }
+    }
+
     function routerHttpMethods(routerFn) {
         return function() {
+
             var middlewareFn = arguments[1];
             var route = arguments[0];
             arguments[1] = function() {
-                console.log('router::start time' + 
-                	(middlewareFn.name || 'route:' + route));
-
+                var $obj = {};
+                $obj["timerObj"] = {};
+                $obj.req = arguments[0];
+                $obj.counter = new Date().getTime();
+                if (!arguments[0].timers) {
+                    arguments[0].timers = [];
+                }
+                console.log('router::start time' +
+                    (middlewareFn.name || 'route:' + route));
                 var nextFn = arguments[2];
                 arguments[2] = function() {
-                    console.log('router::end time' + 
-                    	(middlewareFn.name || 'route:' + route));
+                    console.log('router::end time' +
+                        (middlewareFn.name || 'route:' + route));
 
+                    if (middlewareFn.name) {
+                        $obj["timerObj"][middlewareFn.name] = new Date().getTime() - $obj.counter;
+                    } else {
+                        $obj["timerObj"]["anonymous"] = new Date().getTime() - $obj.counter;
+                    }
+                    $obj.req.timers.push($obj["timerObj"]);
                     nextFn.apply(this, arguments);
+                    
                 }
                 middlewareFn.apply(this, arguments);
             }
-
             return routerFn.apply(this, arguments);
         }
     }
