51be01260715e3050cc10fd11fd2e4457ea028d2
prasanna ramanujam <prasannaraajkumar@gmail.com>
2015-05-16 18:35:12 -0700

    example, test added

---
+++ test/captureTime.js
@@ -0,0 +1,26 @@
+var sinon = require('sinon');
+var assert = require('assert');
+var request = require('supertest');
+var captureTime = require('./../captureTime');
+
+var express = require('express');
+var app = express();
+captureTime(app);
+var middleware = sinon.stub().callsArg(2);
+
+app.use(middleware);
+
+app.get('/', function(req, res, next) {
+	res.status(200).send('ok');
+	next();
+});
+
+describe('app middleware', function() {
+	it('should be called', function(done) {
+		request(app).get('/').expect(200).end(function() {
+			assert.ok(middleware.calledOnce);
+			done();
+		});
+		
+	});
+});
